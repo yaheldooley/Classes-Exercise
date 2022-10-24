6ecd30a665fcd597a1d74824f96588faf6ea09a1
yahel dooley <ydooley@gmail.com>
2022-10-24 14:38:54 -0700

    Created Car class and printed properties to console

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Car car = new Car();
+            car.Make = "Chevy";
+            car.Model = "Bolt";
+            car.Year = 2022;
+
+            Console.WriteLine($"Make: {car.Make}\nModel: {car.Model}\nYear: {car.Year}");
+        }
+
+        public class Car
+        {
+            public string Make;
+            public string Model;
+            public int Year;
         }
     }
 }
